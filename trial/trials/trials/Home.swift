7e89803c81bab860d5af06379cb5a5bea34dbb8f
DivyaKashyap <31915842+DivyaKashyap@users.noreply.github.com>
2018-03-21 04:57:21 +0530

    Add files via upload

---
+++ trial/trials/trials/Home.swift
@@ -0,0 +1,9 @@
+//
+//  Home.swift
+//  trials
+//
+//  Created by Divya Kashyap on 20/03/18.
+//  Copyright © 2018 Divya Kashyap. All rights reserved.
+//
+
+import Foundation
---
+++ trial/trials/trials/SecondVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  trials
+//
+//  Created by Divya Kashyap on 20/03/18.
+//  Copyright © 2018 Divya Kashyap. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func next(_ sender: Any) {
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
